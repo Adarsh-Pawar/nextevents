bd884935ef2ee40f28ee539766f663f4f7fb0b9d
Adarsh Pawar <pawar.adarsh8178@gmail.com>
2024-01-29 16:43:37 +0530

    updating image routes

--- src/components/event-detail/event-logistics.js
+++ src/components/event-detail/event-logistics.js
@@ -17,7 +17,7 @@ function EventLogistics(props) {
   return (
     <section className={classes.logistics}>
       <div className={classes.image}>
-        <img src={`/${image}`} alt={imageAlt} />
+        <img src={`./${image}`} alt={imageAlt} />
       </div>
       <ul className={classes.list}>
         <LogisticsItem icon={DateIcon}>
