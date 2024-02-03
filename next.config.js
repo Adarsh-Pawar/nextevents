2cea384beeb330162f04e962cc3976cdd4fee253
Adarsh Pawar <pawar.adarsh8178@gmail.com>
2024-02-03 17:00:23 +0530

    adding staticgeneration and dynamic data

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
+  output: 'export',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/nextevents/' : '',
 }
 
